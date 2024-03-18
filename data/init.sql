af73b6fa3d9059a65f80428bd20622443747d372
Damiano Bacci <damiano.bacci@gmail.com>
2024-03-18 20:53:59 +0100

    Generate ticket logic

--- data/init.sql
+++ data/init.sql
@@ -4,17 +4,18 @@ CREATE TABLE events (
 	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 	name VARCHAR NOT NULL,
 	date VARCHAR NOT NULL,
-	location VARCHAR NOT NULL
+	location VARCHAR NOT NULL,
+    description VARCHAR NOT NULL
 );
 
 INSERT INTO
 	events
 	(
-		name, date, location
+		name, date, location, description
 	)
 	VALUES
 	(
-		"Tech Conference 2024", datetime('now', '+3 months'), "Silicon Valley Arena"
+		"Tech Conference 2024", datetime('now', '+3 months'), "Silicon Valley Arena", "The premiere tech conference in the world!"
 	)
 ;
 
@@ -25,7 +26,7 @@ INSERT INTO
 	)
 	VALUES
 	(
-		"Tech Conference 2025", datetime('now', '+15 months'), "Silicon Valley Arena"
+		"Tech Conference 2025", datetime('now', '+15 months'), "Silicon Valley Arena", "The premiere tech conference in the world!"
 	)
 ;
 
@@ -36,6 +37,6 @@ INSERT INTO
 	)
 	VALUES
 	(
-		"Tech Conference 2026", datetime('now', '+27 months'), "Silicon Valley Arena"
+		"Tech Conference 2026", datetime('now', '+27 months'), "Silicon Valley Arena", "The premiere tech conference in the world!"
 	)
 ;
--- generate_ticket.php
+++ generate_ticket.php
@@ -1,11 +1,25 @@
 <?php
 
 use chillerlan\QRCode\{QRCode, QROptions};
-
 require_once 'vendor/autoload.php';
+
+$name = $_POST['name'];
 
 $data   = 'https://damianobacci.net';
 $qrcode = (new QRCode)->render($data);
 
-// default output is a base64 encoded data URI
-printf('<img src="%s" alt="QR Code" width="200px" />', $qrcode);
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>A blog application</title>
+        <?php require 'templates/head.php' ?>
+    </head>
+    <body>
+        <h2><?php echo $name ?></h2>
+        <div>
+            <?php printf('<img src="%s" alt="QR Code" width="200px" />', $qrcode); ?>
+        </div>
+    </body>
+</html>
+
